fe068792a119bbcdecfe66d8c66423934d669844
Kame-Bashuna <basshunakameroba@gmail.com>
2024-09-28 10:13:55 +0300

    add files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Landing from "./components/Landing";
-import Camera from "./components/Camera"
-import Camerapermission from "./components/Camerapermission";
-import Process from "./components/Process"
+import Camera from "./components/Camera/page"
+import Camerapermission from "./components/Camerapermission/page";
+import Process from "./components/Process/page"
 export default function Home() {
   return (
     <div >
